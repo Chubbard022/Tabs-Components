873ebe6a4cbd1d9a8af5d68196de21eda3f9f584
chubbard022 <chubbard02@gmail.com>
2019-03-07 12:39:44 -0800

    fixed dropDown bugs

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -2,13 +2,13 @@ class Dropdown {
   constructor(element) {
     
     // Assign this.element to the dropdown element
-    this.element = document.querySelector('.dropdown')
+    this.element = element;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector(".dropdown-button")
+    this.button = this.element.querySelector('.dropdown-button')
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.element.querySelector(".dropdown-button")
+    this.content = this.element.querySelector('.dropdown-content')
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => this.toggleContent())
@@ -17,8 +17,8 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle(".dropdown-hidden")
-    console.log(this.content)
+    this.content.classList.toggle("dropdown-hidden")
+
   }
 }
 
